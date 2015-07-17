c7180fcbbd783a80b92e6286631c75ada869853f
chuyentt <chuyentt@gmail.com>
2015-07-17 22:32:42 +0700

    Added SSZipArchive source

---
+++ Dependencies/SSZipArchive/minizip/aes/entropy.c
@@ -0,0 +1,24 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+
+int entropy_fun(unsigned char buf[], unsigned int len)
+{
+
+    int frand = open("/dev/random", O_RDONLY);
+    ssize_t rlen = 0;
+    if (frand != -1)
+    {
+        rlen = read(frand, buf, len);
+        close(frand);
+    }
+    return rlen;
+}
+    
+#if defined(__cplusplus)
+}
+#endif
